63d3ddd90267bed8b81e83ca0a473fc0198a6031
varun <foo@gmail.com>
2022-12-17 14:24:29 +0530

    Create models for recipes app.

--- recipe/recipes/models.py
+++ recipe/recipes/models.py
@@ -1,3 +1,22 @@
+"""
+Contains model for Recipes app.
+"""
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Recipes(models.Model):
+    """Model for storing recipes."""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return str(self.title)
