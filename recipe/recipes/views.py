a41f5fe1d32c89125e1ea21effa48e5c955245ab
varun <foo@gmail.com>
2022-12-17 22:19:16 +0530

    Create views for Recipe API.

--- recipe/recipes/views.py
+++ recipe/recipes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""
+Contains views for recipes app.
+"""
 
-# Create your views here.
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Recipes
+from .serializers import RecipesSerializer
+
+
+class RecipesViewSet(ModelViewSet):
+    """View set for manage recipes APIs."""
+
+    queryset = Recipes.objects.all()
+    serializer_class = RecipesSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user.
+
+        Only retrieve recipe associated with that particular user.
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-id')
